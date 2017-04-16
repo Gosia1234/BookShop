1dc1fa0d54bede9a30d73bea9701e074dc1d2246
Nick Alexander <nalexanderdev@gmail.com>
2017-04-16 11:55:15 +0000

    add admin account to devise, add_admin_to_users.rb, create user admin@email.ie, update controllers to restrict functionality in items_controller.rb, update_users.rb, add ensure_admin method in items_controller.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416085636) do
+ActiveRecord::Schema.define(version: 20170416105144) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 20170416085636) do
     t.string   "tel"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
